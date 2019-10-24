4a3ec31418b50bd0874fdb7bc6723590d6034a34
DESKTOP-IPI73C4\volch <volchonek@gmail.com>
2019-10-24 23:54:34 +0300

    Рефакторинг кода. Миграция на базу данных.

--- RESTfulAPIService/Startup.cs
+++ RESTfulAPIService/Startup.cs
@@ -1,3 +1,4 @@
+﻿using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -6,21 +7,25 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using RESTfullAPIService.Implementations;
-using RESTfullAPIService.Interfaces;
-using RESTfullAPIService.Models;
+using RESTfulAPIService.Implementations;
+using RESTfulAPIService.Interfaces;
 using Serilog;
 using System.Globalization;
+using System.IO;
+using System.Reflection;
+using RESTfulAPIService.DbContext;
 
-namespace RESTfullAPIService
+namespace RESTfulAPIService
 {
+    /// <summary>
+    /// </summary>
     public class Startup
     {
-        private readonly IConfiguration _configuration;
-
+        /// <summary>
+        /// </summary>
+        /// <param name="configuration"></param>
         public Startup(IConfiguration configuration)
         {
-            _configuration = configuration;
             //Change Level to Debug or smth... 
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Information()
@@ -32,20 +37,18 @@ namespace RESTfullAPIService
                 .CreateLogger();
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// // This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvcCore().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddControllers();
 
-            // Conect to db posgrees
+            // Connect to database
             services.AddDbContextPool<UserDbContext>(options =>
             {
-                // options.UseLoggerFactory(DebugLoggerFactory);
-                // options.UseNpgsql(Configuration.GetConnectionString("Postgress"));
-                // options.UseNpgsql(Configuration.GetSection("ConnectionString")["Postgress"]);
                 options.UseNpgsql("Host = 192.168.1.49; Port = 5433; Database = WebAppService; Username = user; Password = password");   
             });
             
@@ -67,16 +70,29 @@ namespace RESTfullAPIService
 
             // Generate swagger
             services.AddSwaggerGen(swg =>
-                swg.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
                 {
-                    Version = "v1",
-                    Title = "WebApplicationService",
-                    Description = "Testing web application"
+                    swg.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "WebApplicationService",
+                        Description = "Testing web application"
+                    });
+                    
+                swg.DescribeAllEnumsAsStrings();
+                
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                
+                swg.IncludeXmlComments(xmlPath);
                 }
-            ));
+            );
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -91,6 +107,7 @@ namespace RESTfullAPIService
             app.UseSwagger(swg => { swg.RouteTemplate = "/swagger/{documentName}/swagger.json"; });
             app.UseSwaggerUI(swg => { swg.SwaggerEndpoint("/swagger/v1/swagger.json", "Web Application v1"); });
 
+            // TODO:
             // app.UseHttpsRedirection();
             // app.UseAuthorization();
 
