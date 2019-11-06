aa543baca51cc311553f7c203992d991e61a6ce0
DESKTOP-IPI73C4\volch <volchonek@gmail.com>
2019-11-07 00:34:41 +0300

    Added test 'GetByGuidTest200' and 'GetByGuid400'. Todo: GetbyGuid400.

--- RESTfullAPIServiceTest/CotrollersTests/UserControllerTest.cs
+++ RESTfullAPIServiceTest/CotrollersTests/UserControllerTest.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Moq;
 using RESTfulAPIService.Controllers;
 using RESTfulAPIService.Interfaces;
@@ -33,7 +37,7 @@ namespace RESTfullAPIService.CotrollersTests
 //        }
 
         [Fact]
-        public async Task GetAllTest()
+        public async Task GetAllTest200()
         {
             // Arrange - устанавливает начальные условия для выполнения теста
             // создаем заглушку для контроллера эмулируя пработу с репозиторием  
@@ -50,6 +54,41 @@ namespace RESTfullAPIService.CotrollersTests
             var result = Assert.IsType<OkObjectResult>(actionResult);
             // проверяем статус код, должен быть 200.
             Assert.Equal(Microsoft.AspNetCore.Http.StatusCodes.Status200OK, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetByGuidTest200()
+        {
+            // Arrange
+            var iurMockCode = new Mock<IUserRepository>();
+            iurMockCode.Setup(repo => repo.GetByGuid(Guid.Empty));
+            
+            var controller = new UserController(iurMockCode.Object);
+            
+            // Act 
+            var actionResult = await controller.GetByGuid(Guid.Empty);
+            
+            // Assert
+            var result = Assert.IsType<OkResult>(actionResult);
+            Assert.Equal(Microsoft.AspNetCore.Http.StatusCodes.Status200OK, result.StatusCode);
+        }
+        
+        [Fact]
+        public async Task GetByGuidTest400()
+        {
+            // Arrange
+            var iurMockCode = new Mock<IUserRepository>();
+            iurMockCode.Setup(repo => repo.GetByGuid(Guid.Empty));
+
+            var controller = new UserController(iurMockCode.Object);
+
+            // Act 
+            var actionResult = await controller.GetByGuid(new Guid("55388d3f-a2e5-4ee6-97bf-017425ad913"));
+
+            // Assert
+            var result = Assert.IsType<OkResult>(actionResult);
+            
+            Assert.Equal(Microsoft.AspNetCore.Http.StatusCodes.Status400BadRequest, result.StatusCode);
         }
     }
 }
